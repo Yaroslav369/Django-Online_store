d3fc2b534610304e0b72d6b7f710bcb53eadf3eb
Yaroslav <yarlav.maksimov333@gmail.com>
2024-09-26 15:56:11 +0300

    first

---
+++ carts/migrations/0002_alter_cart_options.py
@@ -0,0 +1,15 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('carts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='cart',
+            options={'ordering': ('id',), 'verbose_name': 'Корзина', 'verbose_name_plural': 'Корзина'},
+        ),
+    ]
